c8b660fc33ffe3173ab9eb817815766b0b90dba2
zb <2479107942@qq.com>
2018-10-18 11:18:55 +0800

    添加异步库

--- express/express.js
+++ express/express.js
@@ -1,8 +1,17 @@
 const express=require("express")
+const mongoose=require("mongoose")
+//链接mongo
+const DB_URL='mongodb://127.0.0.1:27017'
+mongoose.connect(DB_URL)
+mongoose.connection.on('connected',()=>{
+    console.log('success')
+})
+
+
 const app=express()
 app.get('/',(req,res)=>{
     res.send("<h1>hello<h1>")
 })
 app.listen(3031,()=>{
-    console.log("监听成功")
+    console.log("监听成功") 
 })
