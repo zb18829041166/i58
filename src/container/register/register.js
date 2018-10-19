4768a80b27e8fcf790f2b20bedd7d0972557c4a3
zb <2479107942@qq.com>
2018-10-19 20:39:26 +0800

    提交注册和登录页面  Please enter the commit message for your changes. Lines starting

--- src/container/register/register.js
+++ src/container/register/register.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import Logo from '../../component/logo/logo'
+import {List,InputItem,WhiteSpace,Button,WingBlank,Radio} from 'antd-mobile'
+
 
 class Register extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            "type":"genius"
+        }
+    }
+    
     render(){
-        return
+        const RadioItem=Radio.RadioItem
+        return (
+            <div>
+                <Logo/>
+                <h2>我是注册页面</h2>
+                <WingBlank>
+                <List>
+                    <InputItem>用户名</InputItem>
+                    <WhiteSpace/>
+                    <InputItem>密码</InputItem> 
+                    <WhiteSpace/>
+                    <InputItem>确认密码</InputItem> 
+                    <WhiteSpace/>
+                    <RadioItem checked={this.state.type==="genius"}>
+                        牛人
+                    </RadioItem>
+                    <WhiteSpace/>
+                    <RadioItem checked={this.state.type==="boss"}>
+                        Boss
+                    </RadioItem>
+                    <Button type='primary'>注册</Button>
+                </List>
+                </WingBlank>
+            </div>
+        )
     }
 }
 
