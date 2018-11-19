59f1280d57665277fef644359738702f174aa549
zb <2479107942@qq.com>
2018-11-19 23:01:13 +0800

    添加eslint标准

--- src/redux/chat.redux.js
+++ src/redux/chat.redux.js
@@ -66,7 +66,7 @@ export function readMsg(from){
        axios.post("/user/readmsg",{from})
        .then(res=>{
            const userid=getState().user._id
-           if(res.status==200&&res.data.code==0){
+           if(res.status===200&&res.data.code===0){
                dispatch(msgRead({userid,from,num:res.data.num}))
            }
        }) 
